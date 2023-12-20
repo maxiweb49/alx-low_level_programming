065fc81f5a98bdf4f53f8e2640a66250dc11ea1f
maxiweb49 <maximax4990@gmail.com>
2023-12-19 21:27:06 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,29 +1,15 @@
 #include "main.h"
 
 /**
-
- * _isalpha - Checks charcter
-
- * @c: The charceter
-
- * Return: 1 for all
-
+ * _isalpha - Checks for alphabetic order
+ *
+ * @c: alpabet to check for
+ *
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 
 int _isalpha(int c)
 
-
 {
-
-        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
-
-        {
-
-                return (1);
-
-        }
-
-        return (0);
-
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
