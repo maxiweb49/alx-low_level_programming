1ae5f169feefcaa555ef9027d4976ee5139a3475
maxiweb49 <maximax4990@gmail.com>
2024-01-05 16:38:45 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
@@ -14,13 +13,16 @@ char *cap_string(char *str)
 	int index = 0;
 
 	while (str[index])
+
+	{
 	while (!(str[index] >= 'a' && str[index] <= 'z'))
 
 	index++;
+
 	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
-	str[index - 1] == ','  ||
+	str[index - 1] == ',' ||
 	str[index - 1] == ';' ||
 	str[index - 1] == '.' ||
 	str[index - 1] == '!' ||
