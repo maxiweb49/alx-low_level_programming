60a0490ff8dffb5e49a298501f6e804cd3cbe437
maxiweb49 <maximax4990@gmail.com>
2024-03-31 14:04:27 -0700

    0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -7,11 +7,11 @@
  * Return: 0 or 1
  */
 
-int _isupper(int b)
+int _isupper(int c)
 
 {
-if (b >= 'A' && b <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+	else
+	return (0);
 }
